885b3753b41e87de4396cc1cae5cbfac2b7b697b
vagrant <vagrant@debian>
2020-06-11 19:50:13 +0000

    primer commit

---
+++ Laboratorio5.js
@@ -0,0 +1,23 @@
+// Operaciones Basicas
+var num1 = 4;
+var num2 = 6;
+console.log("Suma: ", num1 + num2);
+console.log("Resta: ", num1 - num2);
+console.log("Multiplicacion: ", num1 * num2);
+console.log("Division: ", num1 / num2);
+
+//Concatenación
+let cadena1 = "Hola";
+let cadena2 = "Mundo";
+console.log("Concatenacion: " + cadena1+ ' '+ cadena2);
+
+//Tipo de dato
+const const1 = true;
+const const2 = 10;
+
+console.log("Type 1: ",typeof(const1));
+console.log("Type 2: ",typeof(const2));
+
+//Variable Objetos con 4 llaves
+let Objeto ={"number":10, "string":"HolaMundo", "boolean":true, " ":"objeto vacio"};
+console.log("Objetos",Objeto);
